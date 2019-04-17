91d86fded29d464307275a450de75497eb6239e7
Dawid Wdowiak <dawidwdowiak@hotmail.com>
2019-04-18 01:18:42 +0200

    Basic output of the first video frame to jpg

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,34 @@
 #include <iostream>
 #include <string_view>
 
-constexpr std::string_view sample1_path{ R"(C:\Users\Dawid Wdowiak\Desktop\Development\MoviesColorVisualization\build\test_samples\sample1.jpg)" };
+constexpr std::string_view sample1_input{ R"(C:\Users\Dawid Wdowiak\Desktop\Development\MoviesColorVisualization\build\test_samples\sample1.jpg)" };
+constexpr std::string_view sample_input_video{ R"(C:\Users\Dawid Wdowiak\Desktop\Development\MoviesColorVisualization\build\test_samples\sample.mkv)" };
+
+constexpr std::string_view sample_output{ R"(C:\Users\Dawid Wdowiak\Desktop\Development\MoviesColorVisualization\build\test_samples\output.jpg)" };
 
 int main()
 {
-	const auto sample1_image = cv::imread(sample1_path.data());
+	using namespace cv;
 
-	std::cout << sample1_image;
+	VideoCapture video_capture(sample_input_video.data());
+	if (!video_capture.isOpened())
+	{
+		std::cout << "Cannot open the video, either file is corrupted or you are missing some codecs\n";
+		std::cin.get();
+		return -1;
+	}
+
+	for (;;)
+	{
+		Mat frame{};
+		if (!video_capture.read(frame))
+		{
+			break;
+		}
+
+		imwrite(sample_output.data(), frame);
+
+		// tmp
+		break;
+	}
 }
