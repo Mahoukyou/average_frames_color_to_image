77f72e0022650fa632c402457d23524b91ad5d99
Dawid Wdowiak <dawidwdowiak@hotmail.com>
2019-04-18 12:33:57 +0200

    Added working solution
    - average color of each frame in a movie to image

--- src/main.cpp
+++ src/main.cpp
@@ -20,17 +20,38 @@ int main()
 		return -1;
 	}
 
+	std::vector<Scalar> frames_average_color;
+	frames_average_color.reserve(video_capture.get(CAP_PROP_FRAME_COUNT));
+
+	Mat frame{};
+	
+	int counter = 0;
 	for (;;)
 	{
-		Mat frame{};
 		if (!video_capture.read(frame))
 		{
 			break;
 		}
 
-		imwrite(sample_output.data(), frame);
-
-		// tmp
-		break;
+		frames_average_color.push_back(mean(frame));
 	}
+
+	video_capture.release();
+
+	constexpr int height{ 720 };
+	Mat result_image(height, static_cast<int>(frames_average_color.size()), CV_8UC3 );
+	uchar* data = result_image.data;
+	for (int j = 0; j < height; ++j)
+	{
+
+		for (size_t i = 0; i < frames_average_color.size(); ++i)
+		{
+			for (int k = 0; k < 3; ++k)
+			{
+				*data++ = frames_average_color[i][k];
+			}
+		}
+	}
+
+	imwrite(sample_output.data(), result_image);
 }
